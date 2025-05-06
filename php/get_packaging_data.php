6b292e893c90ddcd2f9d4e6933f7e5eefc985fe5
zawad12 <2330031@iub.edu.bd>
2025-05-06 11:21:09 +0600

    get_packaging_data

--- php/get_packaging_data.php
+++ php/get_packaging_data.php
@@ -1,31 +1,45 @@
 <?php
 include "db_connect.php";
 
-$query = "SELECT pk.Package_ID, pk.Processed_Batch_ID, pk.Package_Type, 
-                 pk.Weight, pk.Date_Packaged, pk.ZIP, pk.Storage_Temperature, 
-                 pk.Expiration_Date, p.Quality, b.Batch_ID, t.Name as product_name
-          FROM package pk
-          JOIN processed_meat_batch p ON pk.Processed_Batch_ID = p.Processed_Batch_ID
-          JOIN meat_batch b ON p.Batch_ID = b.Batch_ID
-          JOIN meat_product_grade g ON b.Grade_ID = g.Grade_ID
-          JOIN meat_product_type t ON g.Product_Type_ID = t.Product_Type_ID";
+$query = "
+SELECT 
+    pk.Package_ID, 
+    pk.Processed_Batch_ID, 
+    pk.Package_Type, 
+    pk.Weight, 
+    pk.Date_Packaged, 
+    pk.ZIP, 
+    pk.Storage_Temperature, 
+    pk.Expiration_Date, 
+    p.Quality AS batch_quality, 
+    b.Batch_ID, 
+    t.Name AS product_name
+FROM package pk
+JOIN processed_meat_batch p ON pk.Processed_Batch_ID = p.Processed_Batch_ID
+JOIN meat_batch b ON p.Batch_ID = b.Batch_ID
+JOIN meat_product_grade g ON b.Grade_ID = g.Grade_ID
+JOIN meat_product_type t ON g.Product_Type_ID = t.Product_Type_ID
+";
+
 $result = $conn->query($query);
 
 $data = [];
-while ($row = $result->fetch_assoc()) {
-    $data[] = [
-        'package_id' => $row['Package_ID'],
-        'processed_batch_id' => $row['Processed_Batch_ID'],
-        'package_type' => $row['Package_Type'],
-        'weight' => $row['Weight'],
-        'date_packaged' => $row['Date_Packaged'],
-        'zip' => $row['ZIP'],
-        'storage_temperature' => $row['Storage_Temperature'],
-        'expiration_date' => $row['Expiration_Date'],
-        'quality' => $row['Quality'],
-        'batch_id' => $row['Batch_ID'],
-        'product_name' => $row['product_name']
-    ];
+if ($result && $result->num_rows > 0) {
+    while ($row = $result->fetch_assoc()) {
+        $data[] = [
+            'package_id' => $row['Package_ID'],
+            'batch_id' => $row['Batch_ID'],
+            'processed_batch_id' => $row['Processed_Batch_ID'],
+            'product_name' => $row['product_name'],
+            'package_type' => $row['Package_Type'],
+            'weight' => $row['Weight'],
+            'date_packaged' => $row['Date_Packaged'],
+            'zip' => $row['ZIP'],
+            'storage_temperature' => $row['Storage_Temperature'],
+            'expiration_date' => $row['Expiration_Date'],
+            'quality' => $row['batch_quality']
+        ];
+    }
 }
 
 header('Content-Type: application/json');
