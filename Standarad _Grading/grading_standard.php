1f4b56e43d51f33599592d601456ac6fe0526699
Tasnim <185675524+Tasnim2230434@users.noreply.github.com>
2025-05-07 04:01:40 +0600

    report

--- Standarad _Grading/grading_standard.php
+++ Standarad _Grading/grading_standard.php
@@ -11,7 +11,7 @@
     <!-- SweetAlert2 CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.min.css">
     <!-- Custom CSS -->
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="Standarad_Grading/css/style.css">
 </head>
 <body>
     <!-- Header Section -->
